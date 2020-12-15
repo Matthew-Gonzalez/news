f9a284ef842ebd5745f03823ee62cf08de256472
Matthew Gonzalez <matthewgonzalezmansilla@gmail.com>
2020-12-15 10:25:51 -0300

    feat(Project): add activity_main XML configuration to MainActivity

--- app/src/main/java/cl/ucn/disc/dsn/mgonzalez/news/activities/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsn/mgonzalez/news/activities/MainActivity.java
@@ -47,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    // The Toolbar
+    this.setSupportActionBar(findViewById(R.id.am_t_toolbar));
+
     // The FasterAdapter
     ModelAdapter<News, NewsItem> newsAdapter = new ModelAdapter<>(NewsItem::new);
     FastAdapter<NewsItem> fastAdapter = FastAdapter.with(newsAdapter);
