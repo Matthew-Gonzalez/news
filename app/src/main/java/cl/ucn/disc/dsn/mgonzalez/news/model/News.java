c6739f68548d2df487fa247d8600dd50ab0e033b
pipesalinass <37817401+pipesalinass@users.noreply.github.com>
2021-01-14 10:08:50 -0300

    chore(Project): room imports, add entity, typeconverter for the publishedAt

--- app/src/main/java/cl/ucn/disc/dsn/mgonzalez/news/model/News.java
+++ app/src/main/java/cl/ucn/disc/dsn/mgonzalez/news/model/News.java
@@ -10,6 +10,11 @@
 
 package cl.ucn.disc.dsn.mgonzalez.news.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverter;
+
 import cl.ucn.disc.dsn.mgonzalez.news.utils.Validation;
 import net.openhft.hashing.LongHashFunction;
 import org.threeten.bp.ZonedDateTime;
@@ -19,53 +24,84 @@ import org.threeten.bp.ZonedDateTime;
  *
  * @author Matthew Gonzalez-Mansilla
  */
+@Entity(tableName = "news_table")
 public final class News {
 
   /**
    * Unique id.
    */
-  private Long id;
+  @PrimaryKey(autoGenerate = true)
+  public Long id;
 
   /**
    * The title.
    * Restrictions: not null, size > 2.
    */
+  @ColumnInfo(name = "title")
   private String title;
 
   /**
    * The source.
    */
+  @ColumnInfo(name = "source")
   private String source;
 
   /**
    * The author.
    */
+  @ColumnInfo(name = "author")
   private String author;
 
   /**
    * The URL.
    */
+  @ColumnInfo(name = "url")
   private String url;
 
   /**
    * The image url.
    */
+  @ColumnInfo(name = "urlImage")
   private String urlImage;
 
   /**
    * The description.
    */
+  @ColumnInfo(name = "description")
   private String description;
 
   /**
    * The content.
    */
+  @ColumnInfo(name = "content")
   private String content;
 
   /**
    * The publish date.
    */
-  private ZonedDateTime publishedAt;
+  @ColumnInfo(name = "publishedAt")
+  public ZonedDateTime publishedAt;
+
+  public static class Converters {
+    @TypeConverter
+    public static ZonedDateTime toDate(String dateString) {
+      if (dateString == null) {
+        return null;
+      } else {
+        return ZonedDateTime.parse(dateString);
+      }
+    }
+    @TypeConverter
+    public static String toDateString(ZonedDateTime date) {
+      if (date == null) {
+        return null;
+      } else {
+
+        return date.toString();
+      }
+
+    }
+  }
 
   /**
    * The constructor.
