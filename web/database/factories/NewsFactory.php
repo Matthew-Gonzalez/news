2289417689f0cb929712405e4972ae21c5f13501
Matthew Gonzalez <matthewgonzalezmansilla@gmail.com>
2021-01-19 12:34:05 -0300

    feat(Laravel): add an array of image URLs in the NewsFactory to pick a random one for each news

--- web/database/factories/NewsFactory.php
+++ web/database/factories/NewsFactory.php
@@ -23,6 +23,21 @@ class NewsFactory extends Factory
      */
     public function definition()
     {
+        $imagesArray = [
+            'https://static.zerochan.net/Firis.Mistlud.full.2690752.jpg',
+            'https://static.zerochan.net/Firis.Mistlud.full.2343689.jpg',
+            'https://a.disquscdn.com/uploads/mediaembed/images/3694/5433/original.jpg',
+            'https://i.redd.it/7fti0alwugq21.png',
+            'https://i.redd.it/un5af18egtl21.jpg',
+            'https://pbs.twimg.com/media/Chnoly5XIAEItVX.jpg',
+            'https://pbs.twimg.com/media/C57YigiWcAANXDi.jpg',
+            'https://s3.us-east-2.amazonaws.com/portal-laneta/luisgou/te-perdiste-el-e-3-el-youtuber-alexelcapo-te-lo-muestra-en-su-canal-01-20190619849404940',
+            'https://4.bp.blogspot.com/-UO1zNmz139U/Wl3KsWmFy3I/AAAAAAAAHB4/YmPcJ_C60DA1qJ8WPPmgcB1bVtIvuTevwCLcBGAs/s640/26068208_1544967132205006_3733057124096278528_n.jpg',
+            'https://pbs.twimg.com/media/CycBIFWWgAABjXh.jpg',
+            'https://pbs.twimg.com/media/EPnmc3HWkAACUNT.jpg',
+            'https://pbs.twimg.com/media/EMzRl35WkAElSc3.jpg'
+        ];
+
         $timeZoneID = TimeZone::where('name', 'UTC')->first()->id;
 
         return [
@@ -30,7 +45,7 @@ class NewsFactory extends Factory
             'author' => $this->faker->name,
             'source' => $this->faker->company,
             'url' => $this->faker->unique()->url,
-            'url_image' => $this->faker->imageUrl(),
+            'url_image' => $imagesArray[array_rand($imagesArray)],
             'description' => $this->faker->sentence(10),
             'content' => $this->faker->text(),
             'time_zone_id' => $timeZoneID,
